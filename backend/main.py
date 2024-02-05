ae5de98921052bb3be9afdb75c4aa8f4a31e0d9e
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2024-02-05 22:01:18 +0700

    Modify POST API Done

--- backend/main.py
+++ backend/main.py
@@ -3,22 +3,21 @@ from flask_cors import CORS
 
 # Create a GET route 
 
-
+app = Flask(__name__)
 # Allow CORS
-# CORS(app)
+CORS(app)
 data = [] 
 
-app = Flask(__name__)
+
  
 @app.route('/getData', methods=['GET'])
-def getToDoListData():
+def getData():
     global data
-    data.append(request.args.get('data'))  # Giả sử dữ liệu được truyền qua tham số truy vấn
-    return 'Lưu dữ liệu thành công!', 200
+    return data
 
 
 @app.route('/postData', methods=['POST'])
-def postToDoListData():
-    inputData = request.get_json().get('inputData')  # Get inputData from the JSON request
+def postData():
+    inputData = request.get_json()  # Get inputData from the JSON request
     data.append(inputData)  # Add the inputData to the dataList
     return jsonify({'message': 'Data received successfully', 'dataList': data}), 200
